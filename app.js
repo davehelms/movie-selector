e8574b8b7a2fdd4b9b150b503763f31c22781218
Dave Helms <davehelms@gmail.com>
2018-03-24 15:19:45 -0700

    add first view

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const imdb = require('imdb-api');
-const request = require('request');
+const imdb = require('imdb-api')
+const request = require('request')
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;
+var engine = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+/* Base route for app to load the preference questions*/
+app.get('/', function (req, res) {
+	
+	res.render('preferences')
+
+})
+
+app.get('/movielist', function (req, res) {
+	imdb.get('The Toxic Avenger', {apiKey: process.env.API_KEY, timeout: 30000}).then(function(movie){
+		res.send(movie)
+	}).catch(console.log);
+})
 
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
 
